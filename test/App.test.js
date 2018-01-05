d11eb451729ca116de20c6bffd9867e2503439f8
Jeff Goodall <goodallhj@gmail.com>
2018-01-05 09:08:18 -0700

    Added npm module to allow for testing and updated @import css files

--- test/App.test.js
+++ test/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import App from '../lib/App.js';
-import mockData from "./testMocks.js";
 
 describe('App', () => {
   let wrapper;
@@ -14,19 +13,22 @@ describe('App', () => {
     expect(wrapper).toBeDefined();
   });
 
-  it('should render the h1, triviaList, and Controls component', () => {
+  it('should render the Header, Search, Current, SevenHour, TenDay, and Footer component', () => {
 
-    expect(wrapper.find('h1').length).toEqual(1);
-    expect(wrapper.find('TriviaList').length).toEqual(1);
-    expect(wrapper.find('Controls').length).toEqual(1);
+    expect(wrapper.find('Header').length).toEqual(1);
+    expect(wrapper.find('Search').length).toEqual(1);
+    expect(wrapper.find('Current').length).toEqual(1);
+    expect(wrapper.find('SevenHour').length).toEqual(1);
+    expect(wrapper.find('TenDay').length).toEqual(1);
+    expect(wrapper.find('Footer').length).toEqual(1);
   });
 
-  it('should have default stat of an empty array triviaList', () => {
+  it.skip('should have default stat of an empty array triviaList', () => {
 
     expect(wrapper.state().triviaList).toEqual([]);
   });
 
-  it('should update state', () => {
+  it.skip('should update state', () => {
     expect(wrapper.state().triviaList).toEqual([]);
 
     wrapper.setState({ triviaList: mockData.results });
